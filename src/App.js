2d730f39814b6ab96d1d839dd999a07b40d6a1ff
alqinsidev <padlanalqinsi@live.com>
2022-02-15 16:19:58 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import styled from "@emotion/styled"
+import { Grid, Typography } from "@mui/material";
+import { flexbox } from "@mui/system";
+import React,{useState} from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Div>
+        <Typography variant="h5">Happy Birthday Litha 🎂 </Typography>
+      </Div>
+    </React.Fragment>
   );
 }
+
+const Div = styled.div({
+  display:`flex`,
+  justifyContent:`center`,
+  alignItems:'center',
+  height:'100vh'
+})
 
 export default App;
